17fddec526f2f16338f21965cb11b99a88a97064
alek <>
2025-09-22 20:14:44 +0200

    added needed calculations for the walls' rendering

---
+++ wall_calculations.c
@@ -0,0 +1,48 @@
+#include "./cub3d.h"
+
+void    get_wall_height(t_game *game)
+{
+	game->map.wall_height = WINHEIGHT / game->plr.perp_wall_dist;
+	game->map.draw_start = (-game->map.wall_height / 2) + (WINHEIGHT / 2);
+	if (game->map.draw_start < 0)
+		game->map.draw_start = 0;
+	game->map.draw_end = (game->map.wall_height / 2) + (WINHEIGHT / 2);
+	if (game->map.draw_end >= WINHEIGHT)
+		game->map.draw_end = WINHEIGHT - 1;
+}
+
+void draw_ver_line(t_game *game, int x)
+{
+    int		y;
+    char	*dest;
+
+    if (game->map.grid[game->map.map_x][game->map.map_y])
+    {
+        if (game->map.grid[game->map.map_x][game->map.map_y] == '1')
+            game->map.hex_color = 0xFFFFFF;
+        else if (game->map.grid[game->map.map_x][game->map.map_y] == '0')
+            game->map.hex_color = 0x000000;
+        else
+            game->map.hex_color = 0x32CD32;
+    }
+    if (game->map.side == 1)
+        game->map.hex_color = game->map.hex_color / 2;
+    y = -1;
+    while (++y < game->map.draw_start)
+    {
+        dest = game->win.addr + (y * game->win.line_length + x * (game->win.bits_per_pixel / 8));
+        *(unsigned int*)dest = 0x87CEEB;
+    }
+    y = game->map.draw_start - 1;
+    while (++y <= game->map.draw_end)
+    {
+        dest = game->win.addr + (y * game->win.line_length + x * (game->win.bits_per_pixel / 8));
+        *(unsigned int*)dest = game->map.hex_color;
+    }
+    y = game->map.draw_end;
+    while (++y < WINHEIGHT)
+    {
+        dest = game->win.addr + (y * game->win.line_length + x * (game->win.bits_per_pixel / 8));
+        *(unsigned int*)dest = 0x8B4513;
+    }
+}
